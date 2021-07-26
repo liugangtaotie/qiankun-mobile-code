0299c4fda3671fb08ddd64832ac45181fda9fd1f
互联网-lg <liugang2@bsoft.com.cn>
2021-07-26 22:26:24 +0800

    ADD:增加home页面功能v1.2

--- sub-home/src/views/Message.vue
+++ sub-home/src/views/Message.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <van-nav-bar title="Title" left-text="Back" left-arrow @click-left="$router.go(-1)" />
+    <van-nav-bar title="Message" left-text="Back" left-arrow @click-left="$router.go(-1)" />
     <h1>This is an about message</h1>
     <h1>This is an about message</h1>
     <h1>This is an about message</h1>
