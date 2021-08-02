41e3d71ab68e4e3643dc9978dab386b904010d6f
互联网-lg <liugang2@bsoft.com.cn>
2021-08-02 18:28:01 +0800

    MOD:修改架构v3.7

--- main/vue.config.js
+++ main/vue.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-  transpileDependencies: ['common'],
-  chainWebpack: config => {
-    config.plugin('html')
-      .tap((args) => {
-        args[0].title = 'qiankun-example'
-        return args
-      })
-  }
-}
+  transpileDependencies: ["common"],
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "qiankun-example";
+      return args;
+    });
+  },
+
+  devServer: {
+    port: 8080,
+
+    open: true,
+
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+};
